c7cc58a3ccddce93112cb32bf06b0840928b9d4f
Ruslan Forostianov <ruslan@thehyve.nl>
2014-09-11 17:06:58 +0200

    Make snapshot version unique.

--- TransmartCoreGrailsPlugin.groovy
+++ TransmartCoreGrailsPlugin.groovy
@@ -26,7 +26,7 @@ import org.transmartproject.db.support.DatabasePortabilityService
 
 class TransmartCoreGrailsPlugin {
     // the plugin version
-    def version = "1.0-SNAPSHOT"
+    def version = "1.0-GST-SNAPSHOT"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "2.2 > *"
     // resources that are excluded from plugin packaging
