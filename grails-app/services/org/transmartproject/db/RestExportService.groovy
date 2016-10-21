a3c07ffadee9965aa7466fa4872768127e7c7fc9
Jarvanerp <jarno@thehyve.nl>
2016-10-21 16:02:56 +0200

    Start structering directories of data export

--- grails-app/services/org/transmartproject/db/RestExportService.groovy
+++ grails-app/services/org/transmartproject/db/RestExportService.groovy
@@ -23,21 +23,29 @@ class RestExportService {
     }
 
 
-    File createZip(ArrayList<File> files) {
-        //TODO: Add naming of files based on content of file, instead of a number.
-        Path tmpPath = createTmpDir()
+    File createZip(Path tmpPath) {
         def ant = new AntBuilder()
-        int number = 1
-        files.each { file ->
-            String nameFile = number as String
-            ant.copy(file: file,
-                    tofile: "$tmpPath/$nameFile .txt")
-            number++
-        }
         ant.zip(destfile: "$tmpPath/$ZIP_FILE_NAME",
                 basedir: tmpPath)
         File zipFile = new File("$tmpPath/$ZIP_FILE_NAME")
         zipFile
+    }
+
+    void createDirStructure(ArrayList<File> files, Path tmpPath, Map argument) {
+        //TODO: Use datatype for extra layer inside directory structure and naming of file.
+        //TODO: Add naming of files based on content of file, instead of a number.
+        def ant = new AntBuilder()
+        Map conceptKeys = argument.conceptKeys
+        String resultInstanceIds = (argument.resultInstanceIds.size() > 1) ? argument.resultInstanceIds.join('&'): argument.resultInstanceIds[0].toString()
+        def conceptKey = conceptKeys.values()[0]
+        String studyName = conceptKey.tokenize("\\")[2].toString()
+        files.each { file ->
+            String uuid = UUID.randomUUID().toString().substring(0,5)
+            String typeDir = file.toString().contains('_clinical') ? 'Clinical' : 'Biomarker data'
+            String nameFile = studyName+'_'+typeDir+'_'+uuid
+            ant.copy(file: file,
+                    tofile: "$tmpPath/$studyName"+'_'+"$resultInstanceIds/$typeDir/$nameFile"+".tsv")
+        }
     }
 
     Path createTmpDir() {
@@ -55,71 +63,14 @@ class RestExportService {
     List parseFiles (files, outputFormats) {
         //TODO: Beautify the code, maybe change the location of this code to DataFetchTask?
         List headerNames = []
-        List currentHeaders = []
         List outFiles = []
-        List removableHeaders = []
-        List usedHeaders = []
         Map fileInfo = new HashMap()
         files.each { file ->
-            String fileContents = file.text
-            boolean isBiomarker = false
-            int lineNumber = 1
-            fileContents.eachLine { line ->
-                List lineList = line.split('\t')
-                if (lineNumber == 1) {
-                    lineList.each { rowHeader ->
-                        currentHeaders.add(rowHeader)
-                    }
-                    isBiomarker = '"Bio marker"' in currentHeaders ? true : isBiomarker
-                } else if (isBiomarker){
-                        file in outFiles ? null : outFiles.add(file)
-                        currentHeaders.clear()
-                } else if (!(isBiomarker) && lineNumber > 1) {
-                        String columnName = lineList[0]
-                        if (!fileInfo.containsKey(columnName)) {
-                            fileInfo[columnName] = [:]
-                        }
-                        lineList.each { infoPiece ->
-                            if (infoPiece != "") {
-                                int index = lineList.indexOf(infoPiece)
-                                String currentRowHeader = currentHeaders[index]
-                                usedHeaders.add(currentRowHeader)
-                                try{
-                                    def categoricalHeader = currentRowHeader.tokenize("\\")
-                                    def categoricalValue = '"'+categoricalHeader[-2]+'"'
-                                    if (infoPiece == categoricalValue){
-                                        if (!(categoricalHeader[-3] in currentHeaders)) {
-                                            currentHeaders[index] = categoricalHeader[-3]
-                                        }
-                                        removableHeaders.add(currentRowHeader)
-                                        currentRowHeader = categoricalHeader[-3]
-                                    }
-                                } catch (Exception e){
-
-                                }
-                                def currentInfoMap = fileInfo.get(columnName)
-                                if (currentRowHeader in currentInfoMap) {
-                                    try {
-                                        assert fileInfo[columnName][currentRowHeader] == infoPiece
-                                    } catch(AssertionError e) {
-                                        log.error(e)
-                                    }
-                                } else {
-                                    fileInfo[columnName][currentRowHeader] = infoPiece
-                                }
-                            }
-                        }
-                    }
-                lineNumber++
-            }
-            removableHeaders += currentHeaders-usedHeaders
-            currentHeaders.removeAll(removableHeaders)
-            currentHeaders.each { header ->
-                if (!headerNames.contains(header)) {
-                    headerNames[(headerNames.size())] = header
-                }
-            }
-            currentHeaders = []
+            def returnList = parseFile(file, fileInfo, headerNames)
+            fileInfo = returnList[0]
+            boolean isBiomarker = returnList[1]
+            isBiomarker ? outFiles.add(file) : null
+            headerNames = returnList[2]
         }
         outputFormats.each { outputFormat ->
             switch (outputFormat) {
@@ -131,10 +82,73 @@ class RestExportService {
         outFiles
     }
 
+    def parseFile(File file, Map fileInfo, List headerNames){
+        //TODO: Beautify the code, maybe change the location of this code to DataFetchTask?
+        //TODO: BUG cell type added to used header, deletes it
+        List removableHeaders = []
+        List usedHeaders = []
+        List currentHeaders = []
+        String fileContents = file.text
+        boolean isBiomarker = false
+        int lineNumber = 1
+        fileContents.eachLine { line ->
+            List lineList = line.split('\t')
+            if (lineNumber == 1) {
+                lineList.each { rowHeader ->
+                    currentHeaders.add(rowHeader)
+                }
+                isBiomarker = '"Bio marker"' in currentHeaders ? true : isBiomarker
+            } else if (!(isBiomarker) && lineNumber > 1) {
+                String columnName = lineList[0]
+                if (!fileInfo.containsKey(columnName)) {
+                    fileInfo[columnName] = [:]
+                }
+                lineList.each { infoPiece ->
+                    if (infoPiece != "") {
+                        int index = lineList.indexOf(infoPiece)
+                        String currentRowHeader = currentHeaders[index]
+                        usedHeaders.add(currentRowHeader)
+                        try{
+                            def categoricalHeader = currentRowHeader.tokenize("\\")
+                            def categoricalValue = '"'+categoricalHeader[-2]+'"'
+                            if (infoPiece == categoricalValue){
+                                if (!(categoricalHeader[-3] in currentHeaders)) {
+                                    currentHeaders[index] = categoricalHeader[-3]
+                                    usedHeaders.add(categoricalHeader[-3])
+                                }
+                                removableHeaders.add(currentRowHeader)
+                                currentRowHeader = categoricalHeader[-3]
+                            }
+                        } catch (ArrayIndexOutOfBoundsException e){}
+                        def currentInfoMap = fileInfo.get(columnName)
+                        if (currentRowHeader in currentInfoMap) {
+                            try {
+                                assert fileInfo[columnName][currentRowHeader] == infoPiece
+                            } catch(AssertionError e) {
+                                log.error(e)
+                            }
+                        } else {
+                            fileInfo[columnName][currentRowHeader] = infoPiece
+                        }
+                    }
+                }
+            }
+            lineNumber++
+        }
+        removableHeaders += currentHeaders-usedHeaders
+        currentHeaders.removeAll(removableHeaders)
+        currentHeaders.each { header ->
+            if (!headerNames.contains(header)) {
+                headerNames[(headerNames.size())] = header
+            }
+        }
+        return [fileInfo, isBiomarker, headerNames]
+    }
+
 
     def WriteToTsv(List<String> headerNames, HashMap fileInfo){
         String uuid = UUID.randomUUID().toString()
-        File outFile = new File(System.getProperty("java.io.tmpdir") + uuid + '.txt')
+        File outFile = new File(System.getProperty("java.io.tmpdir") + uuid + '_clinical.txt')
         headerNames.each { header ->
             outFile << header+'\t'
         }
